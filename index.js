096913f5822e717eb3fb9dbbe44811331ecbabec
Rkuangdev <RKuangDev@gmail.com>
2020-11-11 15:40:32 -0500

    add oberservers, and gradients

---
+++ index.js
@@ -0,0 +1,53 @@
+
+var fadeInText = document.querySelectorAll(".fadeIn")
+
+textObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `fadeInAndUp 1s ${entry.target.dataset.delay} ease-out`
+        } // else {
+        //     entry.target.style.animation = `fadeOutAndDown .75s forwards ease-out`
+        // }
+    })
+})
+
+// Text elements fade in and move up
+fadeInText.forEach((element) => {
+    textObserver.observe(element)
+})
+
+
+var slideLeft = document.querySelectorAll(".slideLeft");
+
+sitesObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `slideLeft 1s ease-out`
+        }
+    })
+})
+
+// Text elements fade in and move up
+slideLeft.forEach((element) => {
+    sitesObserver.observe(element)
+})
+
+var slideRight = document.querySelectorAll(".slideRight")
+
+statObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `slideRight 1s ease-out`
+        }
+    })
+})
+
+// Text elements fade in and move up
+slideRight.forEach((element) => {
+    statObserver.observe(element)
+})
+
+//Scroll back to the top on reload
+$(document).ready(function () {
+    $(this).scrollTop(0);
+});
